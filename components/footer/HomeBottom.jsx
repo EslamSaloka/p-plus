f4d7853c8fbefec98f44baeeb1a39b1ff41c78b7
Mohamed Ragab <mohamedragab.cs@gmail.com>
2024-07-12 16:54:14 +0200

    modal

--- components/footer/HomeBottom.jsx
+++ components/footer/HomeBottom.jsx
@@ -6,142 +6,129 @@ import NewsLandingpage from "./NewsLandingpage";
 import DialogModal from "../ui/DialogModal";
 import Image from "next/image";
 import Subscribe from "../ui/Subscribe";
+
 const HomeBottom = ({ imgs, conVersion, desc, news, rtl }) => {
-  const [sliderWidth, setSliderWidth] = useState("180px");
-  const [leftRight, setLeftRight] = useState(false);
-  const [linkProvide, setLinkProvide] = useState("");
-  //setting up for the dialog modal
-  const [open, setOpen] = useState(false);
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const likeAndOpenLink = (link) => {
-    // Perform the action to simulate a "like" (replace with your actual like functionality)
+    const [sliderWidth, setSliderWidth] = useState("180px");
+    const [leftRight, setLeftRight] = useState(false);
+    const [selectedStakholder, setSelectedStakholder] = useState(null);
+    //setting up for the dialog modal
 
-    // Open a new tab or window with the specified URL
-    window.open(link, "_blank");
-  };
-  useEffect(() => {
-    const handleResize = () => {
-      // Adjust the width based on the screen size
-      setSliderWidth(window.innerWidth <= 600 ? "110px" : "180px");
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
     };
 
-    // Set the initial width and add event listener for window resize
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    // Clean up the event listener on component unmount
-    return () => {
-      window.removeEventListener("resize", handleResize);
+    const handleOpen = (stakholder) => {
+        setSelectedStakholder(stakholder); // Set the selected stakholder
+        setOpen(true); // Open the modal
     };
-  }, []);
-  const src = imgs.avatar ? imgs.avatar : "/assets/svg/SANS.svg";
-  return (
-    <div className={classes.homeBottomSectoin}>
-      <NewsLandingpage news={news} rtl={rtl} />
-      <div
-        className={classes.homeBottomMain}
-        id="stakeholder"
-        style={{ direction: rtl ? "rtl" : "ltr" }}
-      >
-        <div
-          className={classes.bottomLogo}
-          style={{ left: rtl ? "-6%" : "10%" }}
-        >
-          <h2
-            style={{
-              direction: rtl ? "rtl" : "",
-              fontFamily: rtl ? "DINNext-Arabic-meduim " : "",
-            }}
-          >
-            <span style={{ fontFamily: rtl ? "DINNext-Arabic-meduim " : "" }}>
-              {rtl ? "أصحاب" : "Our"}
-            </span>{" "}
-            {rtl ? "المصلحة" : "Stackholders"}
-          </h2>
+
+    const likeAndOpenLink = (link) => {
+        // Perform the action to simulate a "like" (replace with your actual like functionality)
+
+        // Open a new tab or window with the specified URL
+        window.open(link, "_blank");
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            // Adjust the width based on the screen size
+            setSliderWidth(window.innerWidth <= 600 ? "110px" : "180px");
+        };
+
+        // Set the initial width and add event listener for window resize
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    return (
+        <div className={classes.homeBottomSection}>
+            <NewsLandingpage news={news} rtl={rtl} />
+            <div className={classes.homeBottomMain} style={{ direction: rtl ? "rtl" : "ltr" }}>
+                <div className={classes.bottomLogo} style={{ left: rtl ? "-6%" : "10%" }}>
+                    <h2 style={{ direction: rtl ? "rtl" : "", fontFamily: rtl ? "DINNext-Arabic-medium" : "" }}>
+                        <span style={{ fontFamily: rtl ? "DINNext-Arabic-medium" : "" }}>
+                            {rtl ? "أصحاب" : "Our"}
+                        </span>{" "}
+                        {rtl ? "المصلحة" : "Stackholders"}
+                    </h2>
+                </div>
+                <div className={classes.stakholderArrows} style={{ direction: "ltr", right: rtl ? "80%" : "10%" }}>
+                    <Image
+                        src="assets/svg/stackholderArrow.svg"
+                        width={25}
+                        height={25}
+                        alt="ArrowLeft"
+                        onClick={() => setLeftRight(false)}
+                        className={classes.stakholderNewsArrow}
+                    />
+                    <Image
+                        src="assets/svg/stackholderArrow.svg"
+                        width={25}
+                        height={25}
+                        alt="ArrowLeft"
+                        onClick={() => setLeftRight(true)}
+                        className={classes.stakholderNewsArrow}
+                    />
+                </div>
+                <div className={classes.partners}>
+                    <Slider
+                        width={sliderWidth}
+                        duration={15}
+                        pauseOnHover={true}
+                        blurBorders={true}
+                        toRight={leftRight}
+                    >
+                        {imgs.map((stakholder, index) => (
+                            <Slider.Slide
+                                key={`${index}${stakholder.name}`}
+                                onClick={() => handleOpen(stakholder)} // Pass stakholder object directly
+                            >
+                                <div className={classes.partnerItem}>
+                                    {stakholder.avatar ? (
+                                        <Image
+                                            src={stakholder.avatar}
+                                            width={140}
+                                            height={140}
+                                            alt={stakholder.title}
+                                            loading="lazy"
+                                        />
+                                    ) : (
+                                        <div
+                                            style={{
+                                                background: "yellow",
+                                                width: 140,
+                                                height: 140,
+                                                borderRadius: "80px",
+                                            }}
+                                        ></div>
+                                    )}
+                                </div>
+                            </Slider.Slide>
+                        ))}
+                    </Slider>
+                </div>
+                {selectedStakholder && (
+                    <DialogModal
+                        open={open}
+                        handleClose={handleClose}
+                        openLink={likeAndOpenLink}
+                        link={selectedStakholder.url}
+                        data={selectedStakholder}
+                        rtl={rtl}
+                    />
+                )}
+            </div>
+            <Subscribe rtl={rtl} />
+            <Footer conVersion={conVersion} desc={desc} rtl={rtl} />
         </div>
-        <div
-          className={classes.stakholderArrows}
-          style={{ direction: "ltr", right: rtl ? "80%" : "10%" }}
-        >
-          <Image
-            src="assets/svg/stackholderArrow.svg"
-            width={25}
-            height={25}
-            alt="ArrowLeft"
-            onClick={() => {
-              setLeftRight(false);
-            }}
-            className={classes.stakholderNewsArrow}
-          />
-          <Image
-            src="assets/svg/stackholderArrow.svg"
-            width={25}
-            height={25}
-            alt="ArrowLeft"
-            onClick={() => {
-              setLeftRight(true);
-            }}
-            className={classes.stakholderNewsArrow}
-          />
-        </div>
-        <div className={classes.partners}>
-          <Slider
-            width={sliderWidth}
-            duration={15}
-            pauseOnHover={true}
-            blurBorders={true}
-            toRight={leftRight}
-          >
-            {imgs.map((stakholder, index) => {
-              return (
-                <Slider.Slide
-                  key={`${index}${stakholder.name}`}
-                  onClick={() => {
-                    handleOpen();
-                    setLinkProvide(stakholder.url);
-                  }}
-                >
-                  <div className={classes.partnerItem}>
-                    {stakholder.avatar ? (
-                      <Image
-                        src={`${stakholder.avatar}`}
-                        width={140}
-                        height={140}
-                        alt={stakholder.title}
-                        loading="lazy"
-                      />
-                    ) : (
-                      <div
-                        style={{
-                          background: "yellow",
-                          width: 140,
-                          height: 140,
-                          borderRadius: "80px",
-                        }}
-                      ></div>
-                    )}
-                  </div>
-                </Slider.Slide>
-              );
-            })}
-          </Slider>
-        </div>
-        <DialogModal
-          open={open}
-          handleClose={handleClose}
-          openLink={likeAndOpenLink}
-          link={linkProvide}
-        />
-      </div>
-      <Subscribe rtl={rtl} />
-      <Footer conVersion={conVersion} desc={desc} rtl={rtl} />
-    </div>
-  );
+    );
 };
 
 export default HomeBottom;
--- components/ui/DialogModal.js
+++ components/ui/DialogModal.js
@@ -1,40 +1,42 @@
 import React from "react";
-import Image from "next/image";
-import classes from "./ui.module.css";
 import { useTranslation } from "react-i18next";
 import Swal from "sweetalert2";
+import classes from "./ui.module.css";
 
-const DialogModal = ({ open, handleClose, openLink, link }) => {
-  const { t, i18n } = useTranslation();
-   const classLang = i18n.language === 'en'? classes.enLang:classes.arLang
-  if (open) { 
-    Swal.fire({
-      icon: "warning",
-      title:  t("alert-header"),
-      text: t("alert-message"),
-      showCancelButton: true,
-      confirmButtonColor: "#4CAF50",
-      cancelButtonColor: "#d33",
-      confirmButtonText: t("confirm"),
-      cancelButtonText: t("cancel"),
-      customClass: {
-        container:  classes.customTitleAlert,
-         title: classes.customTitleAlert,
-         content: classes.customTitleAlert,
-         confirmButton: classes.customTitleAlert,
-         cancelButton: classes.customTitleAlert
-      }
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // Add your custom logic here
-        openLink(link); 
-      }else{
-        handleClose()
-      }
-    });
-  }
+const DialogModal = ({ open, handleClose, openLink, link, data, rtl }) => {
+    const { t } = useTranslation();
 
-  return null;
+    if (open && data) {
+        Swal.fire({
+            icon: data.avatar ? null : "warning",
+            imageUrl: data.avatar || null,
+            imageWidth: 150 || null,
+            imageHeight: 150 || null,
+            imageAlt: 'modal icon' || null,
+            title: data.name || t("alert-header"),
+            text: (rtl ? data.description : data.descriptionEN) || t("alert-message"),
+            showCancelButton: true,
+            confirmButtonColor: "#4CAF50",
+            cancelButtonColor: "#d33",
+            confirmButtonText: t("confirm"),
+            cancelButtonText: t("cancel"),
+            customClass: {
+                container: classes.customTitleAlert,
+                title: classes.customTitleAlert,
+                content: classes.customTitleAlert,
+                confirmButton: classes.customTitleAlert,
+                cancelButton: classes.customTitleAlert,
+            },
+        }).then((result) => {
+            if (result.isConfirmed) {
+                openLink(link);
+            } else {
+                handleClose();
+            }
+        });
+    }
+
+    return null;
 };
 
 export default DialogModal;
